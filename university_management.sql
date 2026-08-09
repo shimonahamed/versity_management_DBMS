a7a6827c33619d2aab2d4c7014601ebb16ad99f3
shimonahamed <smjibonahamed06@gmail.com>
2026-08-09 16:39:11 +0600

    database

---
+++ university_management.sql
@@ -0,0 +1,236 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1deb5ubuntu1
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Aug 09, 2026 at 10:37 AM
+-- Server version: 8.0.46-0ubuntu0.22.04.3
+-- PHP Version: 8.1.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `university_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course`
+--
+
+CREATE TABLE `course` (
+  `id` int UNSIGNED NOT NULL,
+  `course_code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `course_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `department_id` int UNSIGNED NOT NULL,
+  `credit` decimal(3,1) NOT NULL DEFAULT '3.0',
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=Active, 0=Inactive',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `course_registration`
+--
+
+CREATE TABLE `course_registration` (
+  `id` int UNSIGNED NOT NULL,
+  `student_id` int UNSIGNED NOT NULL,
+  `course_id` int UNSIGNED NOT NULL,
+  `semester_id` int UNSIGNED NOT NULL,
+  `session_id` int UNSIGNED NOT NULL,
+  `registration_date` date NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=Active, 0=Inactive',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `department`
+--
+
+CREATE TABLE `department` (
+  `id` int UNSIGNED NOT NULL,
+  `department_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `department_code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `slug` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=Active, 0=Inactive',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `department`
+--
+
+INSERT INTO `department` (`id`, `department_name`, `department_code`, `slug`, `status`, `created_at`, `updated_at`) VALUES
+(3, 'Business Administratiol', 'Nulla est incididun', 'business-administratiol', 1, '2026-08-09 09:29:45', '2026-08-09 09:29:45'),
+(8, 'Business Administrationhj', 'sfdgsj', 'business-administrationhj', 1, '2026-08-09 09:32:38', '2026-08-09 09:32:38'),
+(17, 'Sandra Foley', 'Nemo facere aut nesc', 'Rerum laborum Cupid', 1, '2026-08-09 09:43:09', '2026-08-09 09:43:09');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `semester`
+--
+
+CREATE TABLE `semester` (
+  `id` int UNSIGNED NOT NULL,
+  `semester_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `semester_code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `semester_number` int NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=Active, 0=Inactive',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `session`
+--
+
+CREATE TABLE `session` (
+  `id` int UNSIGNED NOT NULL,
+  `session_name` varchar(100) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `session_code` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=Active, 0=Inactive',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dumping data for table `session`
+--
+
+INSERT INTO `session` (`id`, `session_name`, `session_code`, `status`, `created_at`, `updated_at`) VALUES
+(1, 'Spring-2015', 'Spring-2026', 1, '2026-08-09 09:50:10', '2026-08-09 09:50:10'),
+(3, 'Audra Burke', 'Illum alias at tota', 0, '2026-08-09 09:58:04', '2026-08-09 09:58:04'),
+(4, 'Oscar Mckinney', 'Provident corrupti', 1, '2026-08-09 09:58:11', '2026-08-09 09:58:11');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `students`
+--
+
+CREATE TABLE `students` (
+  `id` int UNSIGNED NOT NULL,
+  `student_id` varchar(50) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `student_name` varchar(150) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `student_email` varchar(150) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `student_phone` varchar(30) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `student_gender` enum('Male','Female','Other') COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `department_id` int UNSIGNED NOT NULL,
+  `session_id` int UNSIGNED NOT NULL,
+  `admission_date` date DEFAULT NULL,
+  `date_of_birth` date DEFAULT NULL,
+  `address` text COLLATE utf8mb4_unicode_ci,
+  `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1=Active, 2=Inactive, 3=Passout, 4=Dropout',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `course`
+--
+ALTER TABLE `course`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `course_code` (`course_code`);
+
+--
+-- Indexes for table `course_registration`
+--
+ALTER TABLE `course_registration`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `department`
+--
+ALTER TABLE `department`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `department_code` (`department_code`),
+  ADD UNIQUE KEY `slug` (`slug`);
+
+--
+-- Indexes for table `semester`
+--
+ALTER TABLE `semester`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `semester_code` (`semester_code`);
+
+--
+-- Indexes for table `session`
+--
+ALTER TABLE `session`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `session_code` (`session_code`);
+
+--
+-- Indexes for table `students`
+--
+ALTER TABLE `students`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `student_id` (`student_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `course`
+--
+ALTER TABLE `course`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `course_registration`
+--
+ALTER TABLE `course_registration`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `department`
+--
+ALTER TABLE `department`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+
+--
+-- AUTO_INCREMENT for table `semester`
+--
+ALTER TABLE `semester`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `session`
+--
+ALTER TABLE `session`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `students`
+--
+ALTER TABLE `students`
+  MODIFY `id` int UNSIGNED NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
