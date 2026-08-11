87ba68073195f1c48c61a20ea7fc8be70aeabce5
shimonahamed <smjibonahamed06@gmail.com>
2026-08-11 14:15:52 +0600

    update

--- includes/header.php
+++ includes/header.php
@@ -13,7 +13,7 @@
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 
     <link rel="stylesheet"
-          href="/university_management/css/style.css">
+          href="/versity_management_DBMS/css/style.css">
 </head>
 
 <body>
